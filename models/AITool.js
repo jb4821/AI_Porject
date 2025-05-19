4726e5bf86e023038152099c8f1ab8f153ef79f2
Jeet <jeet.bhalodiya4@gmail.com>
2025-05-19 20:07:44 +0530

    change schema

--- models/AITool.js
+++ models/AITool.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const AIToolSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
-  category: { type: [String], required: true },
+  category: { type: String, required: true },
   subtitle: { type: String, required: true },
   rating: { type: Number, required: true },
   websiteLink: { type: String, required: true },
   imageLink: { type: String, required: true },
-  features: { type: [String], required: true },
+  features: { type: String, required: true },
   sharableLink: { type: String, required: true }
 });
 
